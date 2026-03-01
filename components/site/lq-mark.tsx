deab436fe24731bc10fbf012cb48a636eddeadbb
jason-zane <jasonzanehunt@gmail.com>
2026-03-02 09:26:11 +1100

    style: update LQ mark to horizontal overlap form

--- components/site/lq-mark.tsx
+++ components/site/lq-mark.tsx
@@ -8,9 +8,9 @@ export function LQMark({ className = '', tone = 'dark' }: LQMarkProps) {
 
   return (
     <span aria-label="Leadership Quarter" className={`inline-block select-none ${toneClass} ${className}`}>
-      <span className="block font-serif text-[2.05rem] font-semibold leading-[1] tracking-[-0.06em]">
-        <span className="block">L</span>
-        <span className="block -mt-[0.56em] pl-[0.08em]">Q</span>
+      <span className="inline-flex items-end font-serif text-[2.1rem] font-semibold leading-[0.88]">
+        <span className="relative z-10">L</span>
+        <span className="-ml-[0.26em] relative z-20">Q</span>
       </span>
     </span>
   )
